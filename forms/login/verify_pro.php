8865f2c6365d08627fd0a817a5e0657feab5226e
pongkitkkpk <pongkitkim@gmail.com>
2023-07-03 23:00:39 +0700

    home and login

---
+++ forms/login/verify_pro.php
@@ -0,0 +1,39 @@
+<?php
+session_start();
+if (isset($_SESSION["id"])) {
+    header("location:../../index.php");
+    die();
+}
+?>
+<?php
+$email = $_POST['email'];
+$p = $_POST['pswd'];
+$conn = new PDO("mysql:host=localhost;dbname=dbbscarbon;charset=utf8", "root", "");
+$sql = " SELECT * FROM member WHERE Email='$email' and password = sha1('$p') ";
+$result = $conn->query($sql);
+if ($result->rowCount() == 1) {
+    date_default_timezone_set('Asia/Bangkok');
+    $time = date("Y-m-d H:i:s");
+    $transec = "INSERT INTO transectionlogin (Email,time) VALUES ('$email','$time')";
+    $conn->exec($transec);
+    $data=$result->fetch(PDO::FETCH_ASSOC);
+    $_SESSION["ID_Company"] = $data["ID_Company"];
+    $_SESSION["Name_Company"] = $data["Name_Company"];
+    $_SESSION["Email"] = $data["Email"];
+    $_SESSION["Phone"] = $data["Phone"];
+    $_SESSION["Detail_Company"] = $data["Detail_Company"];
+    $_SESSION["time"] = $data["time"];
+    $_SESSION["id"] = session_id();
+    header("Location:../../index.php");
+    die();
+} else {
+    $_SESSION["error"] = 1;
+    header("Location:login_page.php");
+    die();
+}
+
+$conn = null;
+?>
+</body>
+
+</html>
---
+++ navbar.php
@@ -0,0 +1,128 @@
+<!-- ยังไม่เข้าสู่ระบบ -->
+<?php if (!isset($_SESSION["ID_Company"])) { ?>
+    <!-- ======= Header ======= -->
+    <header id="header" class="fixed-top ">
+        <div class="container-fluid">
+            <div class="row justify-content-center">
+                <div class="col-xl-9 d-flex align-items-center justify-content-lg-between">
+                    <h1 class="logo me-auto me-lg-0"><a href="templatcopy.php">TP</a></h1>
+                    <!-- Uncomment below if you prefer to use an image logo -->
+                    <!-- <a href="index.html" class="logo me-auto me-lg-0"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+                    <nav id="navbar" class="navbar order-last order-lg-0">
+                        <ul>
+                            <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+                            <li><a class="nav-link scrollto" href="#about">About</a></li>
+                            <li><a class="nav-link scrollto" href="#services">Services</a></li>
+                            <li><a class="nav-link scrollto " href="#portfolio">Portfolio</a></li>
+                            <li><a class="nav-link scrollto" href="#pricing">Pricing</a></li>
+                            <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
+                                <ul>
+                                    <li><a href="#">Drop Down 1</a></li>
+                                    <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
+                                        <ul>
+                                            <li><a href="#">Deep Drop Down 1</a></li>
+                                            <li><a href="#">Deep Drop Down 2</a></li>
+                                            <li><a href="#">Deep Drop Down 3</a></li>
+                                            <li><a href="#">Deep Drop Down 4</a></li>
+                                            <li><a href="#">Deep Drop Down 5</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="#">Drop Down 2</a></li>
+                                    <li><a href="#">Drop Down 3</a></li>
+                                    <li><a href="#">Drop Down 4</a></li>
+                                </ul>
+                            </li>
+                            <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
+                        </ul>
+                        <i class="bi bi-list mobile-nav-toggle"></i>
+                    </nav><!-- .navbar -->
+                    <!-- <a href="#about" class="get-started-btn scrollto">Get Started</a> -->
+                    <a href=""></a>
+                    <a class="get-started-btn scrollto" href="forms/login/login_page.php"> Signup & Login</a>
+
+
+                </div>
+            </div>
+
+        </div>
+    </header><!-- End Header -->
+
+<?php } else { ?>
+    <!-- /*--------------------------------------------------------------
+        # notification
+    --------------------------------------------------------------*/ -->
+    <style>
+        .notification {
+
+            color: white;
+            text-decoration: none;
+            padding: 15px 26px;
+            position: relative;
+            display: inline-block;
+        }
+
+        .notification:hover {
+            color: #009961;
+        }
+
+        .notification .badge {
+            position: absolute;
+            top: 10px;
+            right: px;
+            padding: 5px 10px;
+            border-radius: 50%;
+            background-color: red;
+            color: white;
+        }
+    </style>
+
+    <!-- ======= Header ======= -->
+    <header id="header" class="fixed-top ">
+        <div class="container-fluid">
+            <div class="row justify-content-center">
+                <div class="col-xl-9 d-flex align-items-center justify-content-lg-between">
+                    <h1 class="logo me-auto me-lg-0"><a href="index.php">TP</a></h1>
+                    <!-- Uncomment below if you prefer to use an image logo -->
+                    <!-- <a href="index.html" class="logo me-auto me-lg-0"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+                    <nav id="navbar" class="navbar order-last order-lg-0">
+                        <ul>
+                            <li><a class="nav-link scrollto active" href="personal_info.php"><?php echo $_SESSION['Name_Company']; ?></a></li>
+                            <li><a class="nav-link scrollto" href="#about">About</a></li>
+                            <li><a class="nav-link scrollto" href="#services">Services</a></li>
+                            <li><a class="nav-link scrollto " href="#portfolio">Portfolio</a></li>
+                            <li><a class="nav-link scrollto" href="#pricing">Pricing</a></li>
+                            <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
+                                <ul>
+                                    <li><a href="#">Drop Down 1</a></li>
+                                    <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
+                                        <ul>
+                                            <li><a href="#">Deep Drop Down 1</a></li>
+                                            <li><a href="#">Deep Drop Down 2</a></li>
+                                            <li><a href="#">Deep Drop Down 3</a></li>
+                                            <li><a href="#">Deep Drop Down 4</a></li>
+                                            <li><a href="#">Deep Drop Down 5</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="#">Drop Down 2</a></li>
+                                    <li><a href="#">Drop Down 3</a></li>
+                                    <li><a href="#">Drop Down 4</a></li>
+                                </ul>
+                            </li>
+                            <li><a class="nav-link scrollto" href="#empty">empty</a></li>
+                        </ul>
+                        <i class="bi bi-list mobile-nav-toggle"></i>
+                    </nav><!-- .navbar -->
+                    <a href="#about" class="get-started-btn scrollto">Get Started</a>
+                    <a href="#" class="notification">
+                        <span><i class="bi bi-bell " style="font-size: 20px"></i></span>
+                        <span class="badge">3</span>
+                    </a>
+                    <a href="forms/login/logout_pro.php" class="get-started-btn scrollto">logout</a>
+                </div>
+            </div>
+
+        </div>
+    </header><!-- End Header -->
+<?php   } ?>
