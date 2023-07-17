fc73e2bcb2b85c040e8cebb7147422fc7a87ea7a
Nwpatt <nawapat_link@hotmail.com>
2023-07-17 15:13:49 +0700

    new version Link

--- navbar.php
+++ navbar.php
@@ -5,7 +5,7 @@
         <div class="container-fluid">
             <div class="row justify-content-center">
                 <div class="col-xl-9 d-flex align-items-center justify-content-lg-between">
-                    <h1 class="logo me-auto me-lg-0"><a href="templatcopy.php">CARBONX<i class="bi bi-fire"></i></a></h1>
+                    <h1 class="logo me-auto me-lg-0"><a href="templatcopy.php">CARBONX</a></h1>
                     <!-- Uncomment below if you prefer to use an image logo -->
                     <!-- <a href="index.html" class="logo me-auto me-lg-0"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
 
@@ -13,32 +13,26 @@
                         <ul>
                             <li><a class="nav-link scrollto active" href="#hero">หน้าหลัก</a></li>
                             <li><a class="nav-link scrollto" href="#about">เกี่ยวกับเรา</a></li>
-                            <li><a class="nav-link scrollto" href="#services">บริการของเรา</a></li>
+                            <!-- <li><a class="nav-link scrollto" href="#services">บริการของเรา</a></li> -->
                             <!-- <li><a class="nav-link scrollto " href="#portfolio">Portfolio</a></li>
-                            <li><a class="nav-link scrollto" href="#pricing">Pricing</a></li>
-                            <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
+                            <li><a class="nav-link scrollto" href="#pricing">Pricing</a></li> -->
+                            <li class="dropdown"><a href="#services"><span>บริการของเรา</span> <i class="bi bi-chevron-down"></i></a>
                                 <ul>
-                                    <li><a href="#">Drop Down 1</a></li>
-                                    <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
+                                    <li class="dropdown"><a href="#"><span>ตลาดคาร์บอนเครดิต</span> <i class="bi bi-chevron-right"></i></a>
                                         <ul>
-                                            <li><a href="#">Deep Drop Down 1</a></li>
-                                            <li><a href="#">Deep Drop Down 2</a></li>
-                                            <li><a href="#">Deep Drop Down 3</a></li>
-                                            <li><a href="#">Deep Drop Down 4</a></li>
-                                            <li><a href="#">Deep Drop Down 5</a></li>
+                                            <li><a href="#">การซื้อคาร์บอนเครดิต</a></li>
+                                            <li aria-disabled="true"><a href="#">การขายคาร์บอนเครดิต</a></li>
                                         </ul>
                                     </li>
-                                    <li><a href="#">Drop Down 2</a></li>
-                                    <li><a href="#">Drop Down 3</a></li>
-                                    <li><a href="#">Drop Down 4</a></li>
+                                    <li><a href="#">ประวัติการซื้อของบริษัท</a></li>
+                                    <li><a href="#">คำนวนคาร์บอนเครดิต</a></li>
+                                    <li><a href="#">ข้อมูลส่วนตัวของบริษัท</a></li>
                                 </ul>
-                            </li> -->
-                            <li><a class="nav-link scrollto" href="#contact2">คณะผู้จัดทำ</a></li>
-                            <li><a class="nav-link scrollto" href="#contact">ผู้สนับสนุนหลัก</a></li>
+                            </li>
+                            <li><a class="nav-link scrollto" href="#contact2">คณะผู้จัดการประกวดและผู้สนับสนุนหลัก</a></li>
                         </ul>
                         <i class="bi bi-list mobile-nav-toggle"></i>
-                    </nav><!-- .navbar -->
-                    <!-- <a href="#about" class="get-started-btn scrollto">Get Started</a> -->
+                    </nav>
                     <a href=""></a>
                     <a class="get-started-btn scrollto" href="../login/login_page.php"><i class="bi bi-person-circle"></i> ลงชื่อเช้า / สมัครสมาชิก</a>
 
@@ -49,8 +43,8 @@
         </div>
     </header><!-- End Header -->
 
-<?php } else { 
-    
+<?php } else {
+
 ?>
     <!-- /*--------------------------------------------------------------
         # notification
@@ -85,36 +79,34 @@
         <div class="container-fluid">
             <div class="row justify-content-center">
                 <div class="col-xl-9 d-flex align-items-center justify-content-lg-between">
-                    <h1 class="logo me-auto me-lg-0"><a href="../homepage/home.php">TP</a></h1>
+                    <h1 class="logo me-auto me-lg-0"><a href="templatcopy.php">CARBONX</a></h1>
                     <!-- Uncomment below if you prefer to use an image logo -->
                     <!-- <a href="index.html" class="logo me-auto me-lg-0"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
 
                     <nav id="navbar" class="navbar order-last order-lg-0">
                         <ul>
-                            <li><a class="nav-link scrollto active" href="personal_info.php"><?php echo $_SESSION['Name_Company']; ?></a></li>
-                            <li><a class="nav-link scrollto" href="#about">About</a></li>
-                            <li><a class="nav-link scrollto" href="#services">Services</a></li>
-                            <li><a class="nav-link scrollto " href="#portfolio">Portfolio</a></li>
-                            <li><a class="nav-link scrollto" href="#pricing">Pricing</a></li>
-                            <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
+                            <li><a class="nav-link scrollto active" href="#hero">หน้าหลัก</a></li>
+                            <li><a class="nav-link scrollto" href="#about">เกี่ยวกับเรา</a></li>
+                            <!-- <li><a class="nav-link scrollto" href="#services">บริการของเรา</a></li> -->
+                            <!-- <li><a class="nav-link scrollto " href="#portfolio">Portfolio</a></li>
+                            <li><a class="nav-link scrollto" href="#pricing">Pricing</a></li> -->
+                            <li class="dropdown"><a href="#services"><span>บริการของเรา</span> <i class="bi bi-chevron-down"></i></a>
                                 <ul>
-                                    <li><a href="#">Drop Down 1</a></li>
-                                    <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
+                                    <li class="dropdown"><a href="#"><span>ตลาดคาร์บอนเครดิต</span> <i class="bi bi-chevron-right"></i></a>
                                         <ul>
-                                            <li><a href="#">Deep Drop Down 1</a></li>
-                                            <li><a href="#">Deep Drop Down 2</a></li>
-                                            <li><a href="#">Deep Drop Down 3</a></li>
-                                            <li><a href="#">Deep Drop Down 4</a></li>
-                                            <li><a href="#">Deep Drop Down 5</a></li>
+                                            <li><a href="#">การซื้อคาร์บอนเครดิต</a></li>
+                                            <li><a href="#">การขายคาร์บอนเครดิต</a></li>
                                         </ul>
                                     </li>
-                                    <li><a href="#">Drop Down 2</a></li>
-                                    <li><a href="#">Drop Down 3</a></li>
-                                    <li><a href="#">Drop Down 4</a></li>
+                                    <li><a href="#">ประวัติการซื้อของบริษัท</a></li>
+                                    <li><a href="#">คำนวนคาร์บอนเครดิต</a></li>
+                                    <li><a href="#">ข้อมูลส่วนตัวของบริษัท</a></li>
                                 </ul>
                             </li>
-                            <li><a class="nav-link scrollto" href="../sell/sellpage.php">test sell</a></li>
-                            <li><a class="nav-link scrollto" href="../history/historypage.php">test history</a></li>
+                            <li><a class="nav-link scrollto" href="#contact2">คณะผู้จัดการประกวดและผู้สนับสนุนหลัก</a></li>
+                        <i class="bi bi-list mobile-nav-toggle"></i>
+                        <li><a class="nav-link scrollto" href="../sell/sellpage.php">test sell</a></li>
+                        <li><a class="nav-link scrollto" href="../history/historypage.php">test history</a></li>
                         </ul>
                         <i class="bi bi-list mobile-nav-toggle"></i>
                     </nav><!-- .navbar -->
